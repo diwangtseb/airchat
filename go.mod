7460ecd621981aa2543098f181c2de10387b0bae
diwangtseb <diwangtseb@outlook.com>
2021-10-21 23:05:16 +0800

    add single filght

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-resty/resty/v2 v2.6.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	golang.org/x/net v0.0.0-20211008194852-3b03d305991f // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	google.golang.org/grpc v1.41.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
---
+++ internal/example/singleFlight.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sync/singleflight"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var count int64
+
+func GetArticle(id int64) (string,error){
+	atomic.AddInt64(&count,1)
+	time.Sleep(time.Millisecond*time.Duration(count))
+	return fmt.Sprintf("%d's article is xxx",id),nil
+}
+
+func SingleFligtGetArticle(sg *singleflight.Group,id int64)(string ,error) {
+	v,err,_:= sg.Do(strconv.FormatInt(id,10),func()(interface{}, error){
+		return GetArticle(id)
+	})
+	if err != nil {
+		return "",err
+	}
+	return v.(string),nil
+}
+
+func main(){
+	time.AfterFunc(1*time.Second, func() {
+		atomic.AddInt64(&count,-count)
+	})
+	var (
+		wg sync.WaitGroup
+		now = time.Now()
+		n = 1000
+		sg = &singleflight.Group{}
+	)
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			//res,_ := GetArticle(1)
+			res,_ := SingleFligtGetArticle(sg,1)
+			if res != "1's article is xxx"{
+				panic("err")
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("同时发起%d次请求，耗时:%s\n",n,time.Since(now))
+}
